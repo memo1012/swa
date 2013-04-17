e64d4762ff8c2c1614260b40bede03de879f9ce0
memo1012 <Morten.Mendel@web.de>
2013-04-17 12:14:46 +0200

    was neues

---
+++ shop3/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -0,0 +1,149 @@
+package de.shop.artikelverwaltung.domain;
+
+import java.io.Serializable;
+
+public class Artikel implements Serializable {
+	private static final long serialVersionUID = 1472129607838538329L;
+	
+	// TODO Bean Validation
+	private Long id;
+	
+	// TODO Bean Validation
+	private String bezeichnung;
+	
+	private Kategorie kategorie;
+	private int laenge; //in mm
+	private int breite;
+	private int hoehe;
+	private double gewicht; //in KG
+	private double preis;
+	private boolean verfuegbarkeit;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+	public Kategorie getKategorie() {
+		return kategorie;
+	}
+	public void setKategorie(Kategorie kategorie) {
+		this.kategorie = kategorie;
+	}
+	public int getLaenge() {
+		return laenge;
+	}
+	public void setLaenge(int laenge) {
+		this.laenge = laenge;
+	}
+	public int getBreite() {
+		return breite;
+	}
+	public void setBreite(int breite) {
+		this.breite = breite;
+	}
+	public int getHoehe() {
+		return hoehe;
+	}
+	public void setHoehe(int hoehe) {
+		this.hoehe = hoehe;
+	}
+	public double getGewicht() {
+		return gewicht;
+	}
+	public void setGewicht(double gewicht) {
+		this.gewicht = gewicht;
+	}
+	public double getPreis() {
+		return preis;
+	}
+	public void setPreis(double preis) {
+		this.preis = preis;
+	}
+	public boolean isVerfuegbarkeit() {
+		return verfuegbarkeit;
+	}
+	public void setVerfuegbarkeit(boolean verfuegbarkeit) {
+		this.verfuegbarkeit = verfuegbarkeit;
+	}
+	
+	//Nicht auf Preis und verfugbarkeit
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((bezeichnung == null) ? 0 : bezeichnung.hashCode());
+		result = prime * result + breite;
+		long temp;
+		temp = Double.doubleToLongBits(gewicht);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + hoehe;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((kategorie == null) ? 0 : kategorie.hashCode());
+		result = prime * result + laenge;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Artikel other = (Artikel) obj;
+		if (bezeichnung == null) {
+			if (other.bezeichnung != null)
+				return false;
+		} else if (!bezeichnung.equals(other.bezeichnung))
+			return false;
+		if (breite != other.breite)
+			return false;
+		if (Double.doubleToLongBits(gewicht) != Double
+				.doubleToLongBits(other.gewicht))
+			return false;
+		if (hoehe != other.hoehe)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (kategorie == null) {
+			if (other.kategorie != null)
+				return false;
+		} else if (!kategorie.equals(other.kategorie))
+			return false;
+		if (laenge != other.laenge)
+			return false;
+		return true;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Artikel [id=" + id + ", bezeichnung=" + bezeichnung
+				+ ", kategorie=" + kategorie + ", laenge=" + laenge
+				+ ", breite=" + breite + ", hoehe=" + hoehe + ", gewicht="
+				+ gewicht + ", preis=" + preis + ", verfuegbarkeit="
+				+ verfuegbarkeit + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
+	
+	
---
+++ shop3/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
@@ -0,0 +1,94 @@
+package de.shop.bestellverwaltung.rest;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import java.util.Collection;
+import java.util.Locale;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.UriInfo;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.util.LocaleHelper;
+import de.shop.util.Mock;
+import de.shop.util.NotFoundException;
+
+//JP Code
+import de.shop.artikelverwaltung.domain.Artikel;
+import de.shop.artikelverwaltung.rest.UriHelperArtikel;
+
+//Ende JP
+
+@Path("/bestellungen")
+@Produces(APPLICATION_JSON)
+@Consumes
+public class BestellungResource {
+	@Context
+	private UriInfo uriInfo;
+	
+	@Context
+	private HttpHeaders headers;
+
+	@Inject
+	private UriHelperBestellung uriHelperBestellung;
+	
+		//Anfang JP	Warum gibt es hier eine Bemerkung ?
+		@Inject UriHelperArtikel uriHelperArtikel;
+		//Ende JP
+	
+	
+	
+	@Inject
+	private LocaleHelper localeHelper;
+	
+	@GET
+	@Path("{id:[1-9][0-9]*}")
+	public Bestellung findBestellungById(@PathParam("id") Long id) {
+		@SuppressWarnings("unused")
+		final Locale locale = localeHelper.getLocale(headers);
+		
+		// TODO Anwendungskern statt Mock, Verwendung von Locale
+		final Bestellung bestellung = Mock.findBestellungById(id);
+		if (bestellung == null) {
+			throw new NotFoundException("Keine Bestellung mit der ID " + id + " gefunden.");
+		}
+		
+		// URLs innerhalb der gefundenen Bestellung anpassen
+		uriHelperBestellung.updateUriBestellung(bestellung, uriInfo);
+		return bestellung;
+	}
+	
+	
+	//Anfang JP Code
+	@GET
+	@Path("{id:[1-9][0-9]*}/artikeln")
+	public Collection<Artikel> findArtikelnByBestellungId(@PathParam("id") Long bestellungId) {
+		
+		
+		
+		@SuppressWarnings("unused")
+		final Locale locale = localeHelper.getLocale(headers);
+		
+		// TODO Anwendungskern statt Mock, Verwendung von Locale
+		final Collection<Artikel> artikeln = Mock.findArtikelnByBestellungId(bestellungId);
+		if (artikeln.isEmpty()) {
+			throw new NotFoundException("Zur Bestellung ID " + bestellungId + " wurden keine Artikeln gefunden");
+		}
+		
+		// URLs innerhalb der gefundenen Bestellungen anpassen
+		for (Artikel artikel : artikeln) {
+			uriHelperArtikel.updateUriArtikel(artikel, uriInfo);
+		}
+		
+		return artikeln;
+	}
+	//Ende JP Code
+	
+}
