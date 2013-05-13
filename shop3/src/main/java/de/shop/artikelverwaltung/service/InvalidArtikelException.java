28531bd1c84fae47c679cdcebf57b75dd3a4ffc2
neon67 <neon67fr@gmail.com>
2013-05-13 09:59:43 +0200

    Artikel Exceptions Klassen

---
+++ shop3/src/main/java/de/shop/artikelverwaltung/service/InvalidArtikelException.java
@@ -0,0 +1,11 @@
+package de.shop.artikelverwaltung.service;
+
+import java.util.Collection;
+
+import javax.validation.ConstraintViolation;
+
+import de.shop.kundenverwaltung.domain.AbstractKunde;
+
+public class InvalidArtikelException {
+
+}
